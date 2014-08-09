5df278b073614b9edcc1875341032a7fdff811cb
Cary Robbins <carymrobbins@gmail.com>
2014-08-09 00:59:18 -0500

    Updated cabal plugin to use bash completion.

--- plugins/cabal/cabal.plugin.zsh
+++ plugins/cabal/cabal.plugin.zsh
@@ -1,50 +1,27 @@
-function cabal_sandbox_info() {
-    cabal_files=(*.cabal(N))
-    if [ $#cabal_files -gt 0 ]; then
-        if [ -f cabal.sandbox.config ]; then
-            echo "%{$fg[green]%}sandboxed%{$reset_color%}"
-        else
-            echo "%{$fg[red]%}not sandboxed%{$reset_color%}"
-        fi
-    fi
+autoload bashcompinit
+bashcompinit
+
+# cabal command line completion
+# Copyright 2007-2008 "Lennart Kolmodin" <kolmodin@gentoo.org>
+#                     "Duncan Coutts"     <dcoutts@gentoo.org>
+#
+
+_cabal()
+{
+    # get the word currently being completed
+    local cur
+    cur=${COMP_WORDS[$COMP_CWORD]}
+
+    # create a command line to run
+    local cmd
+    # copy all words the user has entered
+    cmd=( ${COMP_WORDS[@]} )
+
+    # replace the current word with --list-options
+    cmd[${COMP_CWORD}]="--list-options"
+
+    # the resulting completions should be put into this array
+    COMPREPLY=( $( compgen -W "$( ${cmd[@]} )" -- $cur ) )
 }
 
-function _cabal_commands() {
-    # Ensure cabal exists.
-    command -v cabal > /dev/null || return
-    local ret=1 state
-    _arguments ':subcommand:->subcommand' && ret=0
-
-    case $state in
-      subcommand)
-        subcommands=(
-            "--help:Show help text."
-            "--version:Print version information."
-        )
-        for x in $(cabal --list | grep '^[a-z]'); do
-            description=$(
-                # Grep for command.
-                cabal --help | grep "^  $x" | \
-                # Escape quotes.
-                sed "s/'/\\\\'/g" | sed "s/\"/\\\\\"/g" | \
-                # Split on contiguous space and slice from 2nd item onwards.
-                xargs | cut -d ' ' -f 2-)
-            subcommands+=("$x:$description")
-        done
-        for x in $(cabal --list | grep '^\-\-'); do
-            description=$(
-                # Grep for flag, escaping flag dashes.
-                cabal --help | grep "^    $(echo $x | sed 's/-/\\-/g')" | \
-                # Split on contiguous space and slice from 2nd item onwards.
-                xargs | cut -d ' ' -f 2-)
-            if [ "$description" != "" ]; then
-                subcommands+=("$x:$description")
-            fi
-        done
-        _describe -t subcommands 'cabal subcommands' subcommands && ret=0
-    esac
-
-    return ret
-}
-
-compdef _cabal_commands cabal
+complete -F _cabal -o default cabal
